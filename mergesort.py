a02f225a268254b9dff3dc652c0dd7f91f028add
Impri94 <Immo.Primus@gmx.de>
2023-06-18 19:38:37 +0200

    Update mergesort.py with code improvements

--- mergesort.py
+++ mergesort.py
@@ -1,42 +1,44 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
+def merge_sort(lst):
+    """
+    Sortiert eine Liste mit dem Merge-Sort-Algorithmus.
 
+    Args:
+        lst (list): Die zu sortierende Liste.
 
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
+    Returns:
+        None (Die Sortierung erfolgt direkt auf der übergebenen Liste.)
 
-        mergeSort(left)
-        mergeSort(right)
+    """
+    if len(lst) <= 1:
+        return
 
-        l = 0
-        r = 0
-        i = 0
+    mid = len(lst) // 2
+    left = lst[:mid]
+    right = lst[mid:]
 
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
-                l += 1
-            else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
-                r += 1
+    merge_sort(left)
+    merge_sort(right)
+
+    i = j = k = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            lst[k] = left[i]
             i += 1
+        else:
+            lst[k] = right[j]
+            j += 1
+        k += 1
 
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
-            i += 1
+    while i < len(left):
+        lst[k] = left[i]
+        i += 1
+        k += 1
 
-        while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
-            r += 1
-            i += 1
+    while j < len(right):
+        lst[k] = right[j]
+        j += 1
+        k += 1
 
 
 import matplotlib.pyplot as plt
@@ -45,7 +47,9 @@ my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
-mergeSort(my_list)
+
+merge_sort(my_list)
+
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
